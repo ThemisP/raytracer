6874404d3ed1709336170c48fccde8732c0d4a32
Themis <themispapathemistocleous@gmail.com>
2019-02-18 11:40:04 +0000

    direct light, indirect light added and hard shadows

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -31,14 +31,15 @@ vector<Triangle> testScene;
 float focalLength = SCREEN_HEIGHT;
 vec4 camera(0, 0, -3, 1);
 mat4 cameraRotMatrix;
-float yaw = 0.01;
+float yaw = 0.01f;
 
 vec4 lightPos(0, -0.5, -0.7, 1.0);
 vec3 lightColor = 14.0f*vec3(1, 1, 1);
+vec3 indirectLight = 0.5f*vec3(1, 1, 1);
 
 bool Update();
 void Draw(screen* screen);
-bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection);
+bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection, int = (-1));
 void RayTracer(screen* screen);
 void LookAt(const vec3 &from, const vec3 &to);
 vec4 calcDir(int x, int y, vec4 u, vec4 v, vec4 w);
@@ -153,9 +154,9 @@ bool Update() {
 
 
 vec4 calcDir(int x, int y, vec4 u, vec4 v, vec4 w) {
-	float dx = (x - SCREEN_WIDTH * 0.5)*u.x + (SCREEN_HEIGHT*0.5 - y)*v.x - focalLength;
-	float dy = (x - SCREEN_WIDTH * 0.5)*u.y + (SCREEN_HEIGHT*0.5 - y)*v.y - focalLength;
-	float dz = (x - SCREEN_WIDTH * 0.5)*u.z + (SCREEN_HEIGHT*0.5 - y)*v.z - focalLength;
+	float dx = (x - SCREEN_WIDTH * 0.5f)*u.x + (SCREEN_HEIGHT*0.5f - y)*v.x - focalLength;
+	float dy = (x - SCREEN_WIDTH * 0.5f)*u.y + (SCREEN_HEIGHT*0.5f - y)*v.y - focalLength;
+	float dz = (x - SCREEN_WIDTH * 0.5f)*u.z + (SCREEN_HEIGHT*0.5f - y)*v.z - focalLength;
 
 	vec4 dir(dx, dy, dz, 1);
 	return dir;
@@ -177,7 +178,8 @@ void RayTracer(screen* screen) {
 			
 			
 			if (ClosestIntersection(camera, dir, testScene, ClosestIntersectionItem)) {
-				vec3 color = DirectLight(ClosestIntersectionItem);
+				vec3 color = testScene[ClosestIntersectionItem.triangleIndex].color * 
+						(DirectLight(ClosestIntersectionItem) + indirectLight);
 				PutPixelSDL(screen, x, y, color);
 			} else {
 				vec3 color(0, 0, 0);
@@ -189,30 +191,32 @@ void RayTracer(screen* screen) {
 
 }
 
-bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection) {
+bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection, int exclude) {
 	closestIntersection.distance = std::numeric_limits<float>::max();
 	bool result = false;
-	float length = dir.length();
+	float length = glm::length(dir);
 	vec4 direction(dir.x / length, dir.y / length, dir.z / length, 1);
 	for (int i = 0; i < triangles.size(); i++) {
-		Triangle triangle = triangles[i];
-		vec4 v0 = triangle.v0;
-		vec4 v1 = triangle.v1;
-		vec4 v2 = triangle.v2;
-		vec3 e1 = vec3(v1.x - v0.x, v1.y - v0.y, v1.z - v0.z);
-		vec3 e2 = vec3(v2.x - v0.x, v2.y - v0.y, v2.z - v0.z);
-		vec3 b = vec3(start.x - v0.x, start.y - v0.y, start.z - v0.z);
-		mat3 A(-direction, e1, e2);
-		vec3 x = glm::inverse(A) * b;
-		if (x.x >= 0) {
-			if (0 <= x.y && 0 <= x.z && (x.y + x.z) <= 1) {
-				//intersection
-				if (closestIntersection.distance > x.x) {
-					closestIntersection.distance = x.x;
-					closestIntersection.triangleIndex = i;
-					closestIntersection.position = start + direction * x.x;
+		if (i != exclude) {
+			Triangle triangle = triangles[i];
+			vec4 v0 = triangle.v0;
+			vec4 v1 = triangle.v1;
+			vec4 v2 = triangle.v2;
+			vec3 e1 = vec3(v1.x - v0.x, v1.y - v0.y, v1.z - v0.z);
+			vec3 e2 = vec3(v2.x - v0.x, v2.y - v0.y, v2.z - v0.z);
+			vec3 b = vec3(start.x - v0.x, start.y - v0.y, start.z - v0.z);
+			mat3 A(-direction, e1, e2);
+			vec3 x = glm::inverse(A) * b;
+			if (x.x >= 0) {
+				if (0 <= x.y && 0 <= x.z && (x.y + x.z) <= 1) {
+					//intersection
+					if (closestIntersection.distance > x.x) {
+						closestIntersection.distance = x.x;
+						closestIntersection.triangleIndex = i;
+						closestIntersection.position = start + direction * x.x;
+					}
+					result = true;
 				}
-				result = true;
 			}
 		}
 	}
@@ -223,17 +227,24 @@ bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles
 vec3 DirectLight(const Intersection& i) {
 	Triangle triangle = testScene[i.triangleIndex];
 	vec3 P = (vec3)i.position;
-	vec3 r = (((vec3)lightPos) - P);
+	vec3 r = (((vec3)lightPos)-P);
 	vec3 n = glm::normalize(triangle.normal);
 	float angle = acosf(glm::dot(r, n) / (n.length()*r.length()));
-	if (angle < (M_PI/2)) {
-		float rLength = glm::length(r);
+	Intersection intersection;
+	float rLength = glm::length(r);
+	if (ClosestIntersection(vec4(P,1)+vec4(n*0.0001f,1), vec4(r,1), testScene, intersection, i.triangleIndex)) {
+		if (intersection.distance>0.f && intersection.distance < rLength) {
+			return vec3(0, 0, 0);
+		} 
+	}
+	if (angle < (M_PI / 2)) {
 		r = glm::normalize(r);
 		float dotProduct = glm::dot(n, r);
 		if (dotProduct>0) {
-			return (triangle.color)*lightColor * (float)((dotProduct) / (4 * M_PI*rLength*rLength));
-		}		
+			return lightColor * (float)((dotProduct) / (4 * M_PI*rLength*rLength));
+		}
 	}
+	
 
 	return vec3(0,0,0);
 }
